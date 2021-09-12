c35ef334c8c12564a4b3aa7d6c2b9bebfaf2240c
bragavaas <Andre020497!>
2021-09-12 01:03:06 -0300

    updating files with new android config

--- app/src/main/java/com/example/listaalunosactivity/MainActivity.java
+++ app/src/main/java/com/example/listaalunosactivity/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listaalunosactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] alunos = {"Vasco", "Piazzi", "Tiaguin"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
